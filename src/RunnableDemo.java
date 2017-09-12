c6477f2a07c97ae1dac0edc824168b65274e8ff8
Emma Dean <emmadeannow@gmail.com>
2017-09-12 00:23:28 -0400

    practice project for lambdas and anon classes.

---
+++ src/RunnableDemo.java
@@ -0,0 +1,27 @@
+
+public class RunnableDemo {
+    public static void main (String[] args) {
+
+        // !! - Write an anonymous Runnable class here
+        //It can just print out "Anonymous run" or similar
+        Runnable anonRun = new Runnable () {
+            @Override
+            public void run () {
+                System.out.println ("anonymous run");
+            }
+
+        };
+
+
+        anonRun.run ();
+
+        // !! - Write a lambda function here
+        //It can also print out something like "Lambda run"
+        Runnable lambdaRun = () -> {
+            System.out.println ("lambda run");
+        };
+
+        lambdaRun.run ();
+    }
+
+}
